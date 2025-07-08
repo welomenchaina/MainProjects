937001fac755f9470068602bf4f154308493b18e
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-07-08 00:23:42 -0500

    Update SharkEsp.lua

--- SharkEsp.lua
+++ SharkEsp.lua
@@ -3,3 +3,31 @@ local Plr = game.Players.LocalPlayer
 local Char = Plr.Character
 local humanoid = Char.Humanoid
 local hrp = humanoidRootPart
+
+local Window = Luna:CreateWindow({
+	Name = "Shark ESP",
+	Subtitle = "Built with Luna",
+	LogoID = "124233157237852", 
+	LoadingEnabled = true,
+	LoadingTitle = "Loading Shark ESP...",
+	LoadingSubtitle = "Shh, you are my favorite user but.. don't tell the others!",
+
+	ConfigSettings = {
+		ConfigFolder = "SharkESP"
+	},
+
+	KeySystem = false,
+	KeySettings = {
+		Title = "Shark ESP Key",
+		Subtitle = "Enter Key Below",
+		Note = "Join our Discord to get your key.",
+		SaveKey = true,
+		Key = {"your-key-here"},
+		SecondAction = {
+			Enabled = true,
+			Type = "discord",
+			Parameter = "YourDiscordID" -- like "sharkhub" for discord.gg/sharkhub
+		}
+	}
+})
+
