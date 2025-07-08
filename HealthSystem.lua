7559be0de7c58ad596c62b89001f34b02aac99b5
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-07-08 08:10:30 -0500

    Update HealthSystem.lua

--- HealthSystem.lua
+++ HealthSystem.lua
@@ -1,10 +1,3 @@
 local HealthSysFile = "https://rscripts.net/raw/health-system-take-and-sethealth-for-script-developers_1751936776495_sG5nv8AJPN.txt"
 
-local function load(link)
-    loadstring(game:HttpGet(link, true))()
-end
-
-repeat task.wait() until game:IsLoaded()
-
-load(HealthSysFile)
-else
+loadstring(game:HttpGet(HealthSysFile, true))()
