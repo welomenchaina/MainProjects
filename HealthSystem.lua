eab823149202ec0ed4ff88c0abf850af724d7d08
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-07-08 08:11:43 -0500

    Update HealthSystem.lua

--- HealthSystem.lua
+++ HealthSystem.lua
@@ -1,3 +1,3 @@
-local HealthSysFile = "https://rscripts.net/raw/health-system-take-and-sethealth-for-script-developers_1751936776495_sG5nv8AJPN.txt"
 
-loadstring(game:HttpGet(HealthSysFile, true))()
+
+loadstring(game:HttpGet(Https://rscripts.net/raw/health-system-take-and-sethealth-for-script-developers_1751936776495_sG5nv8AJPN.txt, true))()
