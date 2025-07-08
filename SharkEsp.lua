26bb9546b981d483d75e5b0e6efd2f2a21ad1c8f
Hydrox <139654803+welomenchaina@users.noreply.github.com>
2025-07-08 00:46:01 -0500

    Update SharkEsp.lua

--- SharkEsp.lua
+++ SharkEsp.lua
@@ -3,9 +3,27 @@ local Plr = game.Players.LocalPlayer
 local Char = Plr.Character
 local humanoid = Char.Humanoid
 local hrp = humanoidRootPart
+local RunService = game:GetService("RunService")
+
+
+local GameInformation = "https://raw.githubusercontent.com/welomenchaina/GoofyScripts/refs/heads/main/InformationPuller.lua"
+local HealthSystem = "https://raw.githubusercontent.com/welomenchaina/MainProjects/refs/heads/main/HealthSystem.lua"
+-- functions for the health system remember gethealth(), sethealth(), takehealth()
+
+local function load(link)
+    loadstring(game:HttpGet(link, true))()
+end
+
+load(HealthSystem)
+load(GameInformation)
+
+local function getplayersteam()
+return Plr.Team
+end
+
 
 local Window = Luna:CreateWindow({
-	Name = "Shark ESP",
+	Name = "Shark ESP 🦈 | " .. gameName,
 	Subtitle = "Built with Luna",
 	LogoID = "124233157237852", 
 	LoadingEnabled = true,
@@ -26,7 +44,7 @@ local Window = Luna:CreateWindow({
 		SecondAction = {
 			Enabled = true,
 			Type = "discord",
-			Parameter = "YourDiscordID" -- like "sharkhub" for discord.gg/sharkhub
+			Parameter = "YourDiscordID" 
 		}
 	}
 })
