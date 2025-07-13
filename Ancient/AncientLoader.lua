cdee2ce7c1dc0004ceb33d5f4c8be8aa24d1980e
welomenchaina <wm667660@gmail.com>
2025-07-13 01:52:17 -0500

    Ancient Loader Added EZZZ

---
+++ Ancient/AncientLoader.lua
@@ -0,0 +1,151 @@
+-- OMG A SIGMA READING MY CODE OHH NUU
+--[[ 
+██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░▄▀░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█░░░░▄▀░░░░█░░▄▀░░░░░░░░░░█░░▄▀▄▀▄▀▄▀▄▀░░██░░▄▀░░█░░░░░░▄▀░░░░░░█
+█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░▄▀░░██░░▄▀░░█░░▄▀░░███████████░░▄▀░░███░░▄▀░░█████████░░▄▀░░░░░░▄▀░░██░░▄▀░░█████░░▄▀░░█████
+█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░███████████░░▄▀░░███░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█████░░▄▀░░█████
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░███████████░░▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█████░░▄▀░░█████
+█░░▄▀░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█░░▄▀░░███████████░░▄▀░░███░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░██░░▄▀░░█████░░▄▀░░█████
+█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░▄▀░░█░░▄▀░░███████████░░▄▀░░███░░▄▀░░█████████░░▄▀░░██░░▄▀░░░░░░▄▀░░█████░░▄▀░░█████
+█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░░░░░░░░░█░░░░▄▀░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀▄▀▄▀▄▀▄▀░░█████░░▄▀░░█████
+█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░░░░░░░░░▄▀░░█████░░▄▀░░█████
+█░░░░░░██░░░░░░█░░░░░░██████████░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██████████░░░░░░█████░░░░░░█████
+██████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░███░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█
+█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░███░░░░▄▀░░░░█░░▄▀░░░░░░▄▀░░█░░░░░░▄▀░░░░░░█░░▄▀░░█
+█░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░████░░▄▀░░█████░░▄▀░░███░░▄▀░░██░░▄▀░░█████░░▄▀░░█████░░▄▀░░█
+█░░▄▀░░░░░░░░░░█░░▄▀░░█████████░░▄▀░░░░░░░░▄▀░░█████░░▄▀░░███░░▄▀░░░░░░▄▀░░█████░░▄▀░░█████░░▄▀░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀▄▀░░█████░░▄▀░░███░░▄▀▄▀▄▀▄▀▄▀░░█████░░▄▀░░█████░░▄▀░░█
+█░░░░░░░░░░▄▀░░█░░▄▀░░█████████░░▄▀░░░░░░▄▀░░░░█████░░▄▀░░███░░▄▀░░░░░░░░░░█████░░▄▀░░█████░░░░░░█
+█████████░░▄▀░░█░░▄▀░░█████████░░▄▀░░██░░▄▀░░███████░░▄▀░░███░░▄▀░░█████████████░░▄▀░░████████████
+█░░░░░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░░░░░█░░░░▄▀░░░░█░░▄▀░░█████████████░░▄▀░░█████░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀░░█████████████░░▄▀░░█████░░▄▀░░█
+█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░█░░░░░░█████████████░░░░░░█████░░░░░░█
+██████████████████████████████████████████████████████████████████████████████████████████████████
+]]
+
+if not game:IsLoaded() then
+    repeat task.wait() until game:IsLoaded()
+end
+
+local keyFile = "AncientKeySaver.txt"
+
+local luarmor = loadstring(game:HttpGet("https://sdkapi-public.luarmor.net/library.lua"))()
+luarmor.script_id = "f42f3746fb3eb60f837d3673581c14a6"
+
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/welomenchaina/MainProjects/refs/heads/main/Ancient/ObsidianUiLibrary.lua", true))()
+
+local window = UI:CreateWindow({
+    Title = "Ancient Key System",
+    Footer = "Ancient Script",
+    ToggleKeybind = Enum.KeyCode.RightControl,
+    Center = true,
+    AutoShow = true,
+    Resizable = true,
+    Size = UDim2.fromOffset(700, 500)
+})
+
+local keyTab = window:AddKeyTab("Key System")
+local infoTab = window:AddTab("Info", "info")
+local settingsTab = window:AddTab("UI", "settings")
+
+keyTab:AddLabel({ Text = "Ancient Key System", Size = 36, DoesWrap = true })
+
+local function saveKey(key)
+    if writefile then
+        writefile(keyFile, key)
+    end
+end
+
+local function notify(msg, duration)
+    UI:Notify(msg, duration or 4)
+end
+
+local function checkKey(key)
+    local res = luarmor.check_key(key)
+    if res.code == "KEY_VALID" then
+        notify("Key accepted! Welcome aboard.", 5)
+        saveKey(key)
+        return true
+    elseif res.code == "KEY_HWID_LOCKED" then
+        notify("That key's linked to another device. Reset it via the bot.", 5)
+    elseif res.code == "KEY_INCORRECT" then
+        notify("Nope, that key's invalid or deleted.", 5)
+    elseif res.code == "KEY_EXPIRED" then
+        notify("Your key expired. Time to get a new one!", 5)
+    else
+        notify("Key check failed: " .. res.message .. " (" .. res.code .. ")", 5)
+    end
+    return false
+end
+
+
+local savedKey
+if pcall(function() savedKey = readfile(keyFile) end) and savedKey and savedKey ~= "" then
+    if checkKey(savedKey) then
+        keyTab:SetKeyBoxValue(savedKey)
+    else
+        notify("Saved key no good anymore. Please update.", 5)
+    end
+end
+
+keyTab:AddKeyBox("", function(input)
+    checkKey(input)
+end)
+
+keyTab:AddLabel({
+    Text = "\nNo key? Check the <font color='rgb(0, 195, 255)'>Info</font> tab!",
+    DoesWrap = true,
+    Size = 16
+})
+
+local leftBox = infoTab:AddLeftGroupbox("How to Get Your Key", "key")
+leftBox:AddLabel({
+    Text = "Click the button below to get your key!",
+    DoesWrap = true
+})
+
+leftBox:AddButton({
+    Text = "<font color='rgb(255, 172, 28)'>Get Key</font>",
+    Func = function()
+        setclipboard("https://ads.luarmor.net/get_key?for=Ancient_Script-XaLtYHBtNJBV")
+        notify("Copied! Enter This Inside Your Browser!", 3)
+    end
+})
+
+local rightBox = infoTab:AddRightGroupbox("Help", "info")
+rightBox:AddLabel({
+    Text = "Trouble getting a key? Copy our Discord link and ask for help!",
+    DoesWrap = true
+})
+rightBox:AddButton({
+    Text = "Copy Discord",
+    Func = function()
+        setclipboard("https://discord.gg/your-discord-link")
+        notify("Discord link copied!", 3)
+    end
+})
+
+local uiBox = settingsTab:AddLeftGroupbox("Menu", "settings")
+local keyLabel = uiBox:AddLabel("Menu Bind")
+keyLabel:AddKeyPicker("MyKeybind", {
+    Default = "MB2",
+    Text = "Menu Bind",
+    Mode = "Toggle",
+    SyncToggleState = false,
+    Callback = function()
+        UI:Unload()
+    end
+})
+
+uiBox:AddButton({
+    Text = "Close Ancient Script",
+    Func = function()
+        UI:Unload()
+    end,
+    DoubleClick = false
+})
