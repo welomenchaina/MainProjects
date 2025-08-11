74ea457c8db0a4e1f6114e1c32ff245d6c47d1bb
welomenchaina <wm667660@gmail.com>
2025-08-11 02:43:37 -0500

    yh

--- Systems/UniversalLoader.lua
+++ Systems/UniversalLoader.lua
@@ -1,1 +1,5 @@
-local init = 
+local init = loadstring(game:HttpGet("https://raw.githubusercontent.com/welomenchaina/MainProjects/refs/heads/main/Systems/AuraicFunctions.lua",true))()
+
+init:load()
+
+-- working on improving this soon im tired
